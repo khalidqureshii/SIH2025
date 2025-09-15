8ea84cd44be780805b71db3f77104afa479273a2
khalidqureshii <119107500+khalidqureshii@users.noreply.github.com>
2025-09-15 19:21:52 +0530

    Replaced Icon with new icon and added default name to tab

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -52,7 +52,7 @@
 
 
 import { useNavigate } from "react-router-dom";
-import { LogOut, Sprout } from "lucide-react";
+import { LogOut } from "lucide-react";
 import { useTranslation } from "react-i18next";
 import LanguageSelector from "@/components/LanguageSelector";
 import { logoutUser } from "@/store/features/authSlice";
@@ -70,9 +70,14 @@ function Navbar() {
   };
 
   return (
+    // <header
+    //   className="flex justify-between items-center 
+    //              px-6 py-4 bg-white/30 backdrop-blur-md border-b border-white/40 
+    //              shadow-md sticky top-0 z-50"
+    // >
     <header
       className="flex justify-between items-center 
-                 px-6 py-4 bg-white/30 backdrop-blur-md border-b border-white/40 
+                 px-6 py-4 bg-white border-b border-white/40 
                  shadow-md sticky top-0 z-50"
     >
       {/* Logo + Title */}
@@ -81,7 +86,7 @@ function Navbar() {
         className="flex items-center gap-2 text-green-800 hover:opacity-80 transition
                    ltr:order-1 rtl:order-2"
       >
-        <Sprout className="w-7 h-7 rtl:ml-2 rtl:order-1" />
+        <img src="plant.png" className="w-7 h-7 rtl:ml-2 rtl:order-1" />
         <h1 className="text-xl md:text-2xl font-bold tracking-wide rtl:order-1">
           {t("navbar.title")}
         </h1>
