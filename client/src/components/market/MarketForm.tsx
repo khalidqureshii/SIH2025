446a410d9b7c964de84f2ed36b3d10296e76f439
shawn-dcosta <dcostashawn@gmail.com>
2025-09-21 20:11:57 +0530

    Unused import removed at MarketForm

--- client/src/components/market/MarketForm.tsx
+++ client/src/components/market/MarketForm.tsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { Card, CardContent } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { useTranslation } from "react-i18next";
+// import { useTranslation } from "react-i18next";
 
 interface FormProps {
   onSubmit: (data: {
@@ -17,7 +17,7 @@ const MarketForm: React.FC<FormProps> = ({ onSubmit }) => {
   const [district, setDistrict] = useState("");
   const [commodity, setCommodity] = useState("");
 
-  const { t } = useTranslation();
+  // const { t } = useTranslation();
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
@@ -25,39 +25,6 @@ const MarketForm: React.FC<FormProps> = ({ onSubmit }) => {
   };
 
   return (
-    // <Card className="w-full max-w-2xl backdrop-blur-xl bg-white shadow-lg rounded-2xl flex items-center justify-center flex-col">
-    //   <h2 className="text-4xl font-bold text-blue-950 mb-6">
-    //     Market Price Finder
-    //   </h2>
-    //   <CardContent className="p-6">
-    //     <form onSubmit={handleSubmit} className="grid gap-4">
-    //       <Input
-    //         placeholder="Enter State"
-    //         value={state}
-    //         onChange={(e) => setState(e.target.value)}
-    //         className="mt-1 w-full border border-green-400 bg-green-50 rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-green-500 focus:border-green-500"
-    //       />
-    //       <Input
-    //         placeholder="Enter District"
-    //         value={district}
-    //         onChange={(e) => setDistrict(e.target.value)}
-    //         className="mt-1 w-full border border-green-400 bg-green-50 rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-green-500 focus:border-green-500"
-    //       />
-    //       <Input
-    //         placeholder="Enter Commodity"
-    //         value={commodity}
-    //         onChange={(e) => setCommodity(e.target.value)}
-    //         className="mt-1 w-full border border-green-400 bg-green-50 rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-green-500 focus:border-green-500"
-    //       />
-    //       <Button
-    //         type="submit"
-    //         className="bg-blue-600 hover:bg-blue-700 text-white rounded-lg"
-    //       >
-    //         Search Markets
-    //       </Button>
-    //     </form>
-    //   </CardContent>
-    // </Card>
     <Card className="w-full max-w-2xl backdrop-blur-xl bg-white shadow-lg rounded-2xl flex items-center justify-center flex-col mt-6 p-6">
       <h2 className="text-2xl font-bold text-green-700 mb-3 text-center">
         Market Price Finder
