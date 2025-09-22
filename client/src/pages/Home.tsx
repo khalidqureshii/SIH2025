ffa52dd773a7236977403dd47874897fd38620c4
Pankaj Parihar <pnkjparihar38@gmail.com>
2025-09-22 13:47:42 +0530

    Added Plant Identifier

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -143,7 +143,7 @@ const Home = () => {
           >
             <div>
               <img
-                src="/images/weather.jpg"
+                src="/images/thunder.jpg"
                 alt="Weather"
                 className="w-full h-36 object-cover"
               />
@@ -249,6 +249,29 @@ const Home = () => {
               </h3>
               <p className="text-sm text-gray-600">
                 {t("home.descriptions.scheme")}
+              </p>
+            </div>
+          </div>
+
+          <div
+            onClick={() => navigate("/plant-identifier")}
+            className="rounded-xl shadow-lg bg-white/70 backdrop-blur-md 
+               flex flex-col overflow-hidden hover:shadow-2xl hover:scale-[1.02] 
+               transition cursor-pointer"
+          >
+            <div>
+              <img
+                src="/images/plant-identifier.jpeg"
+                alt="Crop Growth Timeline"
+                className="w-full h-36 object-cover"
+              />
+            </div>
+            <div className="flex flex-col flex-1 px-4 py-4">
+              <h3 className="text-lg font-bold text-[rgb(150,75,0)] mb-2">
+                {t("home.shortcut_labels.plant-identifier")}
+              </h3>
+              <p className="text-sm text-gray-600">
+                {t("home.descriptions.plant-identifier")}
               </p>
             </div>
           </div>
