33a8a1e0ab7849e2f250268ea269764eeb877529
shawn-dcosta <dcostashawn@gmail.com>
2025-09-23 21:49:33 +0530

    Auto Reload Issue Resolved V2

--- client/app.js
+++ client/app.js
@@ -10,17 +10,26 @@
 // }
 
 // app.js
-if ("serviceWorker" in navigator) {
-  if (import.meta.env.PROD) {
-    navigator.serviceWorker
-      .register("/sw1.js")
-      .then((reg) => {
-        console.log("Service Worker registered successfully", reg);
-      })
-      .catch((err) => {
-        console.log("Service Worker failed to register", err);
-      });
-  } else {
-    console.log("Service Worker not registered in development mode");
-  }
+// if ("serviceWorker" in navigator) {
+//   if (import.meta.env.PROD) {
+//     navigator.serviceWorker
+//       .register("/sw1.js")
+//       .then((reg) => {
+//         console.log("Service Worker registered successfully", reg);
+//       })
+//       .catch((err) => {
+//         console.log("Service Worker failed to register", err);
+//       });
+//   } else {
+//     console.log("Service Worker not registered in development mode");
+//   }
+// }
+
+
+if ('serviceWorker' in navigator && import.meta.env.PROD) {
+  navigator.serviceWorker.register('/sw.js')
+    .then((reg) => console.log('Service Worker registered', reg))
+    .catch((err) => console.error('Service Worker failed', err));
+} else {
+  console.log('Service Worker not registered in development mode');
 }
