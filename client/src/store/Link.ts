fb47329ca38097c93ccb96d7aca66b5d9e931202
shawn-dcosta <dcostashawn@gmail.com>
2025-09-23 19:14:19 +0530

    Updated Link.ts

--- client/src/store/Link.ts
+++ client/src/store/Link.ts
@@ -1,4 +1,4 @@
 const LINK = "https://sih-2025-backend-two.vercel.app";
 const LINK2 = "https://crop-disease-prediction-v863.onrender.com";
-const LOCAL_LINK = "http://localhost:5000";
+const LOCAL_LINK = "http://localhost:3000";
 export { LINK, LINK2, LOCAL_LINK };
